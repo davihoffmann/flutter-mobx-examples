10a191037eddf44b9902ba8583155082368fe613
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-08-05 19:19:53 -0300

    finalizado exemplo de gerancia de estado com provider

--- lib/providerExample/home.dart
+++ lib/providerExample/home.dart
@@ -1,65 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:flutter_mobx_example/modelosReativos/controller.dart';
+import 'package:flutter_mobx_example/providerExample/body.dart';
+import 'package:flutter_mobx_example/providerExample/controller.dart';
+import 'package:provider/provider.dart';
 
 class MyHomePage extends StatelessWidget {
-  final controller = Controller();
-
-  Widget _textField(
-      {String labelText, onChanged, String Function() errorText}) {
-    return TextField(
-      decoration: InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: labelText,
-        errorText: errorText == null ? null : errorText(),
-      ),
-      onChanged: onChanged,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
+    final controller = Provider.of<ClientController>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Mob X Example"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Observer(builder: (context) {
-                return _textField(
-                    labelText: "Nome",
-                    onChanged: controller.client.changeName,
-                    errorText: controller.validateName);
-              }),
-              SizedBox(height: 15),
-              Observer(builder: (context) {
-                return _textField(
-                    labelText: "E-mail",
-                    onChanged: controller.client.changeEmail,
-                    errorText: controller.validateEmail);
-              }),
-              SizedBox(height: 15),
-              Observer(builder: (context) {
-                return _textField(
-                  labelText: "CPF",
-                  onChanged: controller.client.changeCpf,
-                );
-              }),
-              SizedBox(height: 15),
-              Observer(builder: (context) {
-                return RaisedButton(
-                  onPressed: controller.isValid ? () => {} : null,
-                  child: Text("Salvar"),
-                );
-              })
-            ],
-          ),
+        title: Observer(
+          builder: (context) {
+            return Text(controller.isValid
+                ? "Formulário Validado"
+                : "Formulário não validado");
+          },
         ),
       ),
+      body: BodyWidget(),
     );
   }
 }
